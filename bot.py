3d8ef570eb42d5ba71e867da4351056370d68574
Dmitry <98948895+79773183529@users.noreply.github.com>
2023-02-17 19:11:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-# AnkiAssistantBot
-TOKEN = "6119151293:AAGClhfpBk4qU80-m5F5E8rEPEN00WCkQZE"
+
+TOKEN = ""
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-id_creator = 1068817703
+
